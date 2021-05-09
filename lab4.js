19146081dfbc5c6d64161993be85b6032b2064d0
Alonsovpm <avpizarro@miuandes.cl>
2021-05-09 13:14:44 -0400

    Lab4 listo

--- lab4.js
+++ lab4.js
@@ -1,1 +1,30 @@
-console.log("Hello World");
+var prompt = require('prompt-sync')();
+
+const view = counter =>{
+    let string = `count: ${counter}\n\n(+) (-)\n\n\n(q) for quit\n`;
+    return string;
+}
+
+const update = (msg, counter) =>{
+    if(msg === "+"){
+        return counter + 1;
+    }
+    if(msg === "-"){
+        return counter - 1;
+    }
+}
+
+const app = counter =>{
+    while(true){
+        const currentView = view(counter);
+        console.clear();
+        console.log(view(counter));
+        const msg = prompt("What would you do? ");
+        counter=update(msg, counter);
+        if (msg === "q"){
+            break;
+        }
+    }
+}
+
+app(0);
